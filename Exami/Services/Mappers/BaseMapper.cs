443f4d7ed5d9b3d015b09c11c11ffa702bd36ba9
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-13 04:07:36 +0200

    feat: add Services Project

---
+++ Exami/Services/Mappers/BaseMapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace Services.Mappers;
+
+public abstract class BaseMapper<T> where T : class
+{
+
+    public abstract T MapFromDataRow(DataRow row);
+
+    public List<T> MapFromDataTable(DataTable table)
+    {
+        var result = new List<T>();
+        foreach (DataRow row in table.Rows)
+        {
+            result.Add(MapFromDataRow(row));
+        }
+        return result;
+    }
+}
