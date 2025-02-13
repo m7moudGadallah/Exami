d65b88c2599d4aab339c83df14acdceafdb22068
Abu5ald1 <mohamedkhaled7502@gmail.com>
2025-02-13 13:48:35 +0200

    Entities and Mappers
    I created three entities: {Exam, StudentExam, Subject}
    and their mappers {ExamMapper, StudentExamMapper, SubjectMapper}

---
+++ Exami/Services/Mappers/StudentExamMapper.cs
@@ -0,0 +1,23 @@
+using System.Data;
+using Entities;
+
+namespace Services.Mappers;
+
+public class StudentExamMapper : BaseMapper<StudentExam>
+{
+    public override StudentExam MapFromDataRow(DataRow row)
+    {
+        if (row == null) return null;
+
+        // Convert the DataRow into a StudentExam object
+        return new StudentExam(
+            Id: Convert.ToInt32(row["Id"]),
+            ExamId: Convert.ToInt32(row["ExamId"]),
+            StudentId: Convert.ToInt32(row["StudentId"]),
+            SubmissionTime: row["SubmissionTime"] == DBNull.Value ? (DateTime?)null : Convert.ToDateTime(row["SubmissionTime"]),
+            CreatedAt: row["CreatedAt"] == DBNull.Value ? (DateTime?)null : Convert.ToDateTime(row["CreatedAt"]),
+            UpdatedAt: row["UpdatedAt"] == DBNull.Value ? (DateTime?)null : Convert.ToDateTime(row["UpdatedAt"])
+        );
+    }
+
+}
