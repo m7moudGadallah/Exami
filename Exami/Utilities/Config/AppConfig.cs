ff2d159bc9907c04c408e45613458808527ee722
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-13 02:46:43 +0200

    chore: init solution
    - Add Exami Soluiton
    - Add Presentation project (windows forms)
    - Add Utilities project (class libarary)
    - Add Test project (console app) for manual testing

---
+++ Exami/Utilities/Config/AppConfig.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Utilities.Config;
+public static class AppConfig
+{
+    private static string _configFileName = "appsettings.json";
+    private static IConfigurationRoot _configObj = new ConfigurationBuilder().AddJsonFile(_configFileName).Build();
+
+    public static string ConnectionString { get => _configObj.GetSection("ConnectionString").Value; }
+}
