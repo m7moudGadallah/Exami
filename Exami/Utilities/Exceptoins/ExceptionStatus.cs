8f0d51ca713d995c7042c0553ab1247a72e53f2c
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-14 17:43:55 +0200

    feat: add AppException class for throwing custom exceptions

---
+++ Exami/Utilities/Exceptoins/ExceptionStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Utilities.Exceptoins;
+
+// Define the Status enum to categorize exceptions
+[Flags]
+public enum ExceptionStatus
+{
+    Error,   // Logical error (e.g., invalid input, business rule violation)
+    Fail     // System failure (e.g., database connection issue, external service downtime)
+}
