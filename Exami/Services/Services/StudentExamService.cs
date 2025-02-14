8fcec89a4b199318c3e41a32031a20f1248958a1
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-14 20:40:52 +0200

    feat: add StudentExamService and implement GetStudentExam

---
+++ Exami/Services/Services/StudentExamService.cs
@@ -0,0 +1,26 @@
+﻿using Database;
+using Entities;
+using Services.DTOs;
+using Services.Mappers;
+using System.Data;
+
+namespace Services.Services;
+
+public class StudentExamService
+{
+    public static StudentExam? GetStudentExam(GetStudentExamInputDto data)
+    {
+        var sql = @"
+            SELECT *
+            FROM [StudentExam]
+            WHERE Id = @Id";
+
+        DBCommandParams cmdParams = new(sql, CommandType.Text, new() { ["@Id"] = data.Id });
+
+        var exams = new StudentExamMapper().MapFromDataTable(DatabaseManager.ExecuteDataTable(cmdParams));
+
+        if (exams.Count == 0) return null;
+
+        return exams[0];
+    }
+}
