c08a78038869bd02af923ccdc5e72a5c7998bece
Abu5ald1 <mohamedkhaled7502@gmail.com>
2025-02-14 19:03:19 +0200

    Updated Entities and Mappers

--- Exami/Entities/Exam.cs
+++ Exami/Entities/Exam.cs
@@ -1,4 +1,4 @@
 namespace Entities;
 
-public record Exam(int Id, string? Name, int SubjectId, DateTime? StartTime, 
-                    DateTime? EndTime, string ExamType, string Instructions);
+public record Exam(int Id, string? Name, int? SubjectId, DateTime? StartTime, 
+                    DateTime? EndTime, string ExamType, string? Instructions);
