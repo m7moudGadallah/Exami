6db603122774ba5d6758b95928ca887fce81b7d9
FatmaMahmoud22 <fatmamhmoud20230@gmail.com>
2025-02-14 18:18:34 +0200

    add entities

---
+++ Exami/Entities/ExamQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    internal class ExamQuestion
+    {
+       int ExamId { get; set; }
+        public int QuestionId { get; set; }
+       
+    }
+}
---
+++ Exami/Entities/StudentAnswer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    internal class StudentAnswer
+    {
+        int StudentExam {  get; set; }
+        int AnswerId {  get; set; }
+        DateTime CreatedAt { get; set; }
+    }
+}
