bfb87a752f2d9ed3c4dc41bd41aecacce831f9a1
FatmaMahmoud22 <fatmamhmoud20230@gmail.com>
2025-02-14 18:52:47 +0200

    Add Answer,StudentAnswer,Question, MarkedQuestion,ExamQuestion Entities and thier mapper

---
+++ Exami/Services/Mappers/QuestionMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using Entities;
+
+namespace Services.Mappers
+{
+    public class QuestionMapper : BaseMapper<Question>
+    {
+        public override Question MapFromDataRow(DataRow row)
+        {
+            if (row == null) return null;
+
+           
+            return new Question(
+                Id: Convert.ToInt32(row["Id"]),
+                Mark: Convert.ToDouble(row["Mark"]),
+                Body: row["Body"] == DBNull.Value ? null : row["Body"].ToString(),
+                QuestionType: row["QuestionType"].ToString(),
+                SubjectId: Convert.ToInt32(row["SubjectId"])
+            );
+        }
+    }
+}
