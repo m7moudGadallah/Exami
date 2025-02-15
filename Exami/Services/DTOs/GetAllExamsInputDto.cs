13cc974da9252a8cd7d4625e04bb0af4f0111071
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-15 11:06:33 +0200

    feat: update ExamService - add GetAllExams method

---
+++ Exami/Services/DTOs/GetAllExamsInputDto.cs
@@ -0,0 +1,3 @@
+﻿namespace Services.DTOs;
+
+public record GetAllExamsInputDto(Dictionary<string, object> Filters);
--- Exami/Services/Services/ExamService.cs
+++ Exami/Services/Services/ExamService.cs
@@ -4,6 +4,7 @@ using Entities;
 using Services.DTOs;
 using Services.Mappers;
 using Utilities.Exceptoins;
+using System.Text;
 
 namespace Services.Services;
 
@@ -12,6 +13,49 @@ namespace Services.Services;
 /// </summary>
 public class ExamService
 {
+    /// <summary>
+    /// Retrieves a list of exams based on the provided filters.
+    /// </summary>
+    /// <param name="dto">A <see cref="GetAllExamsInputDto"/> object containing key-value pairs of filters to apply.</param>
+    /// <returns>A list of <see cref="Exam"/> objects that match the specified filters.</returns>
+    /// <exception cref="AppException">
+    /// Thrown if an error occurs during the database operation.
+    /// </exception>
+    public static List<Exam> GetAllExams(GetAllExamsInputDto dto)
+    {
+        try
+        {
+            var sql = new StringBuilder(@"
+                SELECT *
+                FROM [Exam]
+                WHERE 1=1");
+
+
+            DBCommandParams cmdParams = new(sql.ToString(), CommandType.Text, new() { });
+
+            // Add filters dynamically
+            foreach (var filter in dto.Filters)
+            {
+                string columnName = filter.Key;
+                object value = filter.Value;
+
+                if (value != null)
+                {
+                    sql.Append($" AND {columnName} = @{columnName}");
+                    cmdParams.Parameters.Add($"@{columnName}", value);
+                }
+            }
+
+            var exams = new ExamMapper().MapFromDataTable(DatabaseManager.ExecuteDataTable(cmdParams with { Sql = sql.ToString() }));
+
+            return exams;
+        }
+        catch (Exception ex)
+        {
+            throw new AppException(ex.Message, ExceptionStatus.Fail, ex.InnerException);
+        }
+    }
+
     /// <summary>
     /// Retrieves an exam by its unique identifier.
     /// </summary>
