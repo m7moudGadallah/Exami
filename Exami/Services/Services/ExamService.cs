d657f0fd5700951dd0e3db8ca57f8c0d143d2612
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-20 21:04:08 +0200

    feat: update ExamiSerivce - remove GetExam method

--- Exami/Services/Services/ExamService.cs
+++ Exami/Services/Services/ExamService.cs
@@ -82,35 +82,4 @@ public static class ExamService
             throw new AppException(ex.Message, ExceptionStatus.Fail, ex.InnerException);
         }
     }
-
-    /// <summary>
-    /// Retrieves an exam by its unique identifier.
-    /// </summary>
-    /// <param name="dto">A <see cref="GetExamInputDto"/> object containing the ID of the exam to retrieve.</param>
-    /// <returns>A <see cref="Exam"/> object if an exam with the specified ID exists; otherwise, <see langword="null"/>.</returns>
-    /// <exception cref="AppException">
-    /// Thrown if an error occurs during the database operation.
-    /// </exception>
-    public static Exam GetExam(GetExamInputDto dto)
-    {
-        try
-        {
-            var sql = @"
-            SELECT *
-            FROM [Exam]
-            WHERE Id = @Id";
-
-            DBCommandParams cmdParams = new(sql, CommandType.Text, new() { ["@Id"] = dto.Id });
-
-            var exams = new ExamMapper().MapFromDataTable(DatabaseManager.ExecuteDataTable(cmdParams));
-
-            if (exams.Count == 0) return null;
-
-            return exams[0];
-        }
-        catch (Exception ex)
-        {
-            throw new AppException(ex.Message, ExceptionStatus.Fail, ex.InnerException);
-        }
-    }
 }
