1cd086da16497df72c15befa68ee89d29508ca20
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-20 20:48:25 +0200

    feat: update Exam and Subject entities by addin navigation properties

--- Exami/Entities/Exam.cs
+++ Exami/Entities/Exam.cs
@@ -1,4 +1,7 @@
 namespace Entities;
 
 public record Exam(int Id, string Name, int? SubjectId, DateTime StartTime,
-                    DateTime EndTime, ExamType ExamType, string? Instructions);
+                    DateTime EndTime, ExamType ExamType, string? Instructions)
+{
+    public Subject Subject { get; set; }
+}
