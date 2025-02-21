4aa96de966bb297acbeb326f3448f8de1edcba3d
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-21 11:22:36 +0200

    feat: add UserService - add GetAllUsers method

---
+++ Exami/Services/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using Database;
+using System.Data;
+using Entities;
+using Services.DTOs;
+using Services.Mappers;
+using Utilities.Exceptoins;
+
+namespace Services.Services;
+
+public static class UserService
+{
+    public static List<User> GetAllUsers(GetAllUsersInputDto? dto)
+    {
+        try
+        {
+            var sql = @"
+                SELECT *
+                FROM [User]
+                WHERE 1=1";
+
+            SqlQueryBuilder queryBuilder = new(new(sql, CommandType.Text, new() { }));
+
+            // Add filters dynamically
+            if (dto?.Filters != null)
+            {
+                queryBuilder.ApplyFilters(dto.Filters);
+            }
+
+            // Add ordering
+            if (dto?.OrderBy == null || dto.OrderBy.Count == 0)
+            {
+                dto.OrderBy = new() { ["Id"] = 1 };
+            }
+
+            queryBuilder.ApplyOrderBy(dto.OrderBy);
+
+            // Add pagination
+            if (dto?.Skip != null && dto?.Take != null && dto.Skip >= 0 && dto.Take > 0)
+            {
+                queryBuilder.ApplyPagination(dto.Take.Value, dto.Skip.Value);
+            }
+
+            var users = new UserMapper().MapFromDataTable(DatabaseManager.ExecuteDataTable(queryBuilder.CmdParams));
+
+            return users;
+        }
+        catch (Exception ex)
+        {
+            throw new AppException(ex.Message, ExceptionStatus.Fail, ex.InnerException);
+        }
+    }
+}
