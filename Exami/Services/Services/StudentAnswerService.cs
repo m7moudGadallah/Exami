e871832a4a3f20cb195174b49f4cb6f61c348685
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-21 18:56:08 +0200

    refactor: enhance StudentAnswerService

--- Exami/Services/Services/StudentAnswerService.cs
+++ Exami/Services/Services/StudentAnswerService.cs
@@ -8,74 +8,26 @@ using Utilities.Exceptoins;
 
 namespace Services.Services;
 
-/// <summary>
-/// Provides methods to manage student answers, including retrieving a list of student answers based on filters, creating new student answers, and deleting existing student answers.
-/// </summary>
-public static class StudentAnswerService
+public class StudentAnswerService : CRUDService<StudentAnswer>
 {
-    /// <summary>
-    /// Retrieves a list of student answers based on the provided filters.
-    /// </summary>
-    /// <param name="dto">A <see cref="GetAllStudentAnswersInputDto"/> object containing key-value pairs of filters to apply.</param>
-    /// <returns>A list of <see cref="StudentAnswer"/> objects that match the specified filters.</returns>
-    /// <exception cref="AppException">
-    /// Thrown if an error occurs during the database operation or if invalid filters are provided.
-    /// </exception>
-    public static List<StudentAnswer> GetAllAnswers(GetAllStudentAnswersInputDto dto)
+    public StudentAnswerService() : base("StudentAnswer", new StudentAnswerMapper()) { }
+
+    public override StudentAnswer Create(StudentAnswer dto)
     {
         try
         {
-            var sql = new StringBuilder(@"
-                SELECT *
-                FROM [StudentAnswer]
-                WHERE 1=1");
-
-
-            DBCommandParams cmdParams = new(sql.ToString(), CommandType.Text, new() { });
-
-            // Add filters dynamically
-            foreach (var filter in dto.Filters)
-            {
-                string columnName = filter.Key;
-                object value = filter.Value;
-
-                if (value != null)
-                {
-                    sql.Append($" AND {columnName} = @{columnName}");
-                    cmdParams.Parameters.Add($"@{columnName}", value);
-                }
-            }
-
-            var stdAnswers = new StudentAnswerMapper().MapFromDataTable(DatabaseManager.ExecuteDataTable(cmdParams with { Sql = sql.ToString() }));
-
-            return stdAnswers;
-        }
-        catch (Exception ex)
-        {
-            throw new AppException(ex.Message, ExceptionStatus.Fail, ex.InnerException);
-        }
-    }
-
-    /// <summary>
-    /// Creates a new student answer and returns the created record.
-    /// </summary>
-    /// <param name="dto">A <see cref="CreateStudentAnswerInputDto"/> object containing the details of the student answer to create.</param>
-    /// <returns>The newly created <see cref="StudentAnswer"/> object.</returns>
-    /// <exception cref="AppException">
-    /// Thrown if an error occurs during the database operation or if the input data is invalid.
-    /// </exception>
-    public static StudentAnswer CreateStudentAnswer(CreateStudentAnswerInputDto dto)
-    {
-        try
-        {
-            var sql = @"
-                INSERT INTO [StudentAnswer] (StudentExamId, AnswerId, CreatedAt)
+            var sql = $@"
+                INSERT INTO [{_tableName}] (StudentExamId, AnswerId, CreatedAt)
                 OUTPUT INSERTED.*
                 VALUES (@StudentExamId, @AnswerId, GETDATE());";
 
-            DBCommandParams cmdParams = new(sql, CommandType.Text, new() { ["@StudentExamId"] = dto.StudentExamId, ["@AnswerId"] = dto.AnswerId });
+            DbCommandParams cmdParams = new(sql, CommandType.Text, new()
+            {
+                ["@StudentExamId"] = dto.StudentExamId,
+                ["@AnswerId"] = dto.AnswerId
+            });
 
-            var stdAnswer = new StudentAnswerMapper().MapFromDataTable(DatabaseManager.ExecuteDataTable(cmdParams))[0];
+            var stdAnswer = _mapper.MapFromDataTable(_dbContext.ExecuteDataTable(cmdParams))[0];
 
             return stdAnswer;
         }
@@ -85,32 +37,8 @@ public static class StudentAnswerService
         }
     }
 
-    /// <summary>
-    /// Deletes a student answer based on the provided input.
-    /// </summary>
-    /// <param name="dto">A <see cref="DeleteStudentAnswerInputDto"/> object containing the details of the student answer to delete.</param>
-    /// <returns><see langword="true"/> if the deletion was successful; otherwise, <see langword="false"/>.</returns>
-    /// <exception cref="AppException">
-    /// Thrown if an error occurs during the database operation or if the input data is invalid.
-    /// </exception>
-    public static bool DeleteStudentAnswer(DeleteStudentAnswerInputDto dto)
+    public override StudentAnswer Update(StudentAnswer dto)
     {
-        try
-        {
-            var @sql = @"
-                DELETE [StudentAnswer]
-                WHERE StudentExamId = @StudentExamId AND AnswerId = @AnswerId";
-
-
-            DBCommandParams cmdParams = new(sql, CommandType.Text, new() { ["@StudentExamId"] = dto.StudentExamId, ["@AnswerId"] = dto.AnswerId });
-
-            int rowsAffected = DatabaseManager.ExecuteNonQuery(cmdParams);
-
-            return rowsAffected > 0;
-        }
-        catch (Exception ex)
-        {
-            throw new AppException(ex.Message, ExceptionStatus.Fail, ex.InnerException);
-        }
+        throw new NotImplementedException();
     }
 }
