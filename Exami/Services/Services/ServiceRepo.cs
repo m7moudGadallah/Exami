cb1280681e696e8b010595f96e5166544b5446bc
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-21 18:59:52 +0200

    refactor: add ServiceRepo class

---
+++ Exami/Services/Services/ServiceRepo.cs
@@ -0,0 +1,18 @@
+﻿namespace Services.Services;
+
+public static class ServicesRepo
+{
+    private static Dictionary<Type, Service> _services = new();
+
+    public static T GetService<T>() where T : Service, new()
+    {
+        Type serviceType = typeof(T);
+
+        if (!_services.ContainsKey(serviceType) || _services[serviceType] == null)
+        {
+            _services[serviceType] = new T();
+        }
+
+        return (T)_services[serviceType];
+    }
+}
