4ce25ee6ad14a7ca747c044211ab0cff314cacae
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-21 22:35:20 +0200

    Revert "yes"
    This reverts commit d66176da7059bad4a818f173b7034a85a8e01dae.

--- Exami/Presentation/Forms/StudentMainForm.Designer.cs
+++ Exami/Presentation/Forms/StudentMainForm.Designer.cs
@@ -5,7 +5,7 @@ using MaterialColorScheme = MaterialSkin.ColorScheme;
 
 namespace Presentation
 {
-    internal partial class StudentMainForm
+    partial class StudentMainForm
     {
         /// <summary>
         /// Required designer variable.
--- Exami/Presentation/Forms/StudentMainForm.cs
+++ Exami/Presentation/Forms/StudentMainForm.cs
@@ -1,4 +1,4 @@
-﻿using Entities;
+using Entities;
 using Services.DTOs;
 using Services.Services;
 using Presentation.Helpers;
