9d826b7a1fd492a0666f386cee82b3d26f8090e9
Mahmoud Gadallah <gadallah@std.mans.edu.eg>
2025-02-21 18:55:05 +0200

    refactor: enhance AnswerService

--- Exami/Services/Services/AnswerService.cs
+++ Exami/Services/Services/AnswerService.cs
@@ -1,58 +1,19 @@
-﻿using Database;
-using Entities;
-using Services.DTOs;
+﻿using Entities;
 using Services.Mappers;
-using System.Data;
-using System.Text;
-using Utilities.Exceptoins;
 
 namespace Services.Services;
 
-/// <summary>
-/// Provides methods to manage answers, including retrieving a list of answers based on filters.
-/// </summary>
-public static class AnswerService
+public class AnswerService : CRUDService<Answer>
 {
-    /// <summary>
-    /// Retrieves a list of answers based on the provided filters.
-    /// </summary>
-    /// <param name="dto">A <see cref="GetAllAnswersInputDto"/> object containing key-value pairs of filters to apply.</param>
-    /// <returns>A list of <see cref="Answer"/> objects that match the specified filters.</returns>
-    /// <exception cref="AppException">
-    /// Thrown if an error occurs during the database operation or if invalid filters are provided.
-    /// </exception>
-    public static List<Answer> GetAllAnswers(GetAllAnswersInputDto dto)
+    public AnswerService() : base("Answer", new AnswerMapper()) { }
+
+    public override Answer Create(Answer dto)
     {
-        try
-        {
-            var sql = new StringBuilder(@"
-                SELECT *
-                FROM [Answer]
-                WHERE 1=1");
+        throw new NotImplementedException();
+    }
 
-
-            DBCommandParams cmdParams = new(sql.ToString(), CommandType.Text, new() { });
-
-            // Add filters dynamically
-            foreach (var filter in dto.Filters)
-            {
-                string columnName = filter.Key;
-                object value = filter.Value;
-
-                if (value != null)
-                {
-                    sql.Append($" AND {columnName} = @{columnName}");
-                    cmdParams.Parameters.Add($"@{columnName}", value);
-                }
-            }
-
-            var answers = new AnswerMapper().MapFromDataTable(DatabaseManager.ExecuteDataTable(cmdParams with { Sql = sql.ToString() }));
-
-            return answers;
-        }
-        catch (Exception ex)
-        {
-            throw new AppException(ex.Message, ExceptionStatus.Fail, ex.InnerException);
-        }
+    public override Answer Update(Answer dto)
+    {
+        throw new NotImplementedException();
     }
 }
